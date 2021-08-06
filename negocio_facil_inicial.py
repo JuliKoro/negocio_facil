d64dcb2c674ee7822c938a893ac177a51bd67325
Julián Koroluk <julian.koroluk@outlook.com>
2021-08-06 16:04:51 -0300

    Create negocio_facil_inicial.py
    Se creo el archivo de programa Python (.py)

---
+++ negocio_facil_inicial.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+'''
+Negocio Fácil [Python]
+(Prototipo inicial)
+---------------------------
+Descripcion:
+Programa creado como proyecto inicial para administrar un negocio comercial 
+en diversas funciones.
+Para más información leer el archivo "README.md"
+'''
+
+__author__ = "Julián Andrés Koroluk"
+__email__ = "julian.koroluk@outlook.com"
+__version__ = "0.1"
+
+
+def crear_perfil():
+    pass
+
+
+def cargar_perfil():
+    pass
+
+
+if __name__ == '__main__':
+    print('¡Bienvenido a Tu Negocio Fácil!\nUn programa para ayudarte a administrar',
+    ' tu negocio en diversas tareas.\n')
+    
